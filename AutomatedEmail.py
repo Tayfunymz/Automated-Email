5603c99ddf7c342e7c5594af1bdc8bc9c937f2c2
Tayfunymz <74079478+Tayfunymz@users.noreply.github.com>
2020-12-04 11:21:59 -0800

    Update AutomatedEmail.py

--- AutomatedEmail.py
+++ AutomatedEmail.py
@@ -6,7 +6,7 @@ import smtplib, ssl
 
 
 def log_in_to_google_sheets():
-    scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+    scope = []
     creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
     client = gspread.authorize(creds)
     sheet = client.open("tutorial").sheet1
